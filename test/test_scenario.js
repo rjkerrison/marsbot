6ae9eb4d0938172e28a3bdec80f578198dc9d28c
Robin James Kerrison <robin.kerrison@reedonline.co.uk>
2019-09-28 23:02:30 +0100

    Adding test case for scenario outputting robot movement; test fails

--- test/test_scenario.js
+++ test/test_scenario.js
@@ -10,4 +10,14 @@ describe('Scenario', function() {
 
     assert.deepEqual(new Grid(5, 3), scenario.grid)
   })
+
+  it('should move a robot', function() {
+    const scenario = new Scenario()
+
+    scenario.addGridLine('5 3')
+    scenario.addRobotLines('1 1 E', 'FRF')
+    const output = scenario.execute()
+
+    assert.deepEqual('2 0 S', output)
+  })
 })
