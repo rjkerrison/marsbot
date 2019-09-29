d3a807604bb5fee92d41da9763af3f5095b32203
Robin James Kerrison <robin.kerrison@reedonline.co.uk>
2019-09-29 11:07:53 +0100

    Adding command limit and grid limit functionality

--- src/scenario.js
+++ src/scenario.js
@@ -3,34 +3,57 @@ const Robot = require('./robot')
 const { getVector, getCompass } = require('./orientation')
 
 class Scenario {
-  constructor({maxNorth, maxEast, maxCommands}) {
+  constructor(config) {
+    if (config) {
+      const {maxNorth, maxEast, maxCommands} = config
+      this.maxNorth = maxNorth
+      this.maxEast = maxEast
+      this.maxCommands = maxCommands
+    }
+
     this.robots = []
-    this.maxNorth = maxNorth
-    this.maxEast = maxEast
-    this.maxCommands = maxCommands
   }
 
   addGridLine(line) {
-    const [x, y] = line.split(' ')
+    const [x, y] = this._applyGridLimits(...intFromString(...line.split(' ')))
     this.grid = new Grid(x, y)
   }
 
-  addRobotLines(position, moves) {
+  addRobotLines(position, commands) {
     const [x_string, y_string, c] = position.split(' ')
-    const [x, y] = intFromString(x_string, y_string)
+    const [x, y] = this._applyGridLimits(...intFromString(x_string, y_string))
 
     const robot = new Robot(x, y, {...getVector(c)}, this.grid)
-    this.robots.push({robot, moves})
+    commands = this._applyCommandsLimit(commands)
+
+    this.robots.push({robot, commands})
   }
 
   execute() {
-    const outputLines = this.robots.map(function({robot, moves}) {
-      robot.move_all(moves)
+    const outputLines = this.robots.map(function({robot, commands}) {
+      robot.move_all(commands)
 
       return `${robot.x} ${robot.y} ${getCompass(robot.o)}${robot.isLost ? ' LOST' : ''}`
     })
 
     return outputLines
+  }
+
+  _applyGridLimits(x, y) {
+    if (this.maxEast) {
+      x = Math.min(x, this.maxEast)
+    }
+    if (this.maxNorth) {
+      y = Math.min(y, this.maxNorth)
+    }
+    return [x, y]
+  }
+
+  _applyCommandsLimit(commands) {
+    if (this.maxCommands) {
+      return commands.substring(this.maxCommands)
+    }
+    return commands
   }
 }
 
